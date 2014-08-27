c958cd5e8fa2100ff9e9adc8809d53f241019ccd
Stuart Yamartino <stuart@yamartino.com>
2014-08-27 00:32:39 -0700

    All added all the shirt images & finished "about"

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ var d = 0;
 var ampm = 0;
 var minute = 0;
 var second = 0;
+var shirt = 0;
 
 $(document).ready(function(){
+	startTime();
 	$('#top h1').addClass('animated bounceInDown');
 	$('h2').addClass('animated bounceInUp');
 	$('#shirt').addClass('animated fadeIn');
-	startTime();
 });
 
 function startTime(){
@@ -16,6 +17,33 @@ function startTime(){
 	minute = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
 	second = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds();
 	$('h2').text(((d.getHours() + 11) % 12 + 1) + ':' + minute + ':' + second + ' ' + ampm);
+
+	if(d.getHours() <= 8 || d.getHours() == 9)
+	{
+		shirt = 1;
+	}
+	else if(d.getHours() == 10 || d.getHours() == 11)
+	{
+		shirt = 2;
+	}
+	else if(d.getHours() == 12 || d.getHours() == 13)
+	{
+		shirt = 3;
+	}
+	else if(d.getHours() == 14 || d.getHours() == 15)
+	{
+		shirt = 4;
+	}
+	else if(d.getHours() == 16 || d.getHours() == 17)
+	{
+		shirt = 5;
+	}
+	else
+	{
+		shirt = 6;
+	}
+
+	$('#shirt').attr('src', 'img/' + shirt + '.png');
 	setTimeout(startTime, 1000);
 }
 
